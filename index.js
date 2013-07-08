ca52f257f846e08b443a8c58075bf0c1ce15b977
whirlibulf <whirlibulf.engine@gmail.com>
2013-07-08 16:07:22 +0100

    Update for new engine API, remove history file

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 function System(options) {
+    this.engine = null;
+    this.index = null;
 }
 
 System.prototype.init = function (engine) {
-  console.log("Kinematics system loaded");
-  this.engine = engine;
+    console.log("Kinematics system loaded");
+    this.engine = engine;
+    this.index = this.engine.index(["velocity", "position"]);
 };
 
 System.prototype.update = function (dt) {
-  var objects, i, velocity, position;
+    var entities, i, velocity, position;
 
-  dt /= 1000;
+    dt /= 1000;
 
-  objects = this.engine.getAll('velocity');
+    entities = this.engine.getAll(this.index);
 
-  for (i = 0; i < objects.length; ++i) {
-    velocity = this.engine.get(objects[i], 'velocity');
-    position = this.engine.get(objects[i], 'position');
+    for (i = 0; i < entities.length; ++i) {
+        velocity = this.engine.get(entities[i], "velocity");
+        position = this.engine.get(entities[i], "position");
 
-    if (position === undefined) {
-      continue;
+        position.x += (velocity.x || 0) * dt;
+        position.y += (velocity.y || 0) * dt;
+        position.z += (velocity.z || 0) * dt;
     }
-
-    position.x += (velocity.x || 0) * dt;
-    position.y += (velocity.y || 0) * dt;
-    position.z += (velocity.z || 0) * dt;
-  }
 };
 
 module.exports = System;
